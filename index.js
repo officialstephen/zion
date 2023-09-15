6af4f9f631fb459103fbfdba7213562623ce99e5
Stephen Adegbola <adegbola@fisshboneandlestr.com>
2023-09-15 10:21:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ Hello: "World" });
+});
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
