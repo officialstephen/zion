6867c2f776819ad8ae6d1b3ee3ea3c7c76f28a30
Stephen Adegbola <adegbola@fisshboneandlestr.com>
2023-09-15 12:39:13 +0100

    update with deployment script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Navigate to your project directory (change the path as needed)
+cd .
+
+# Ensure you're using the correct version of Node.js (optional)
+# nvm use <node_version>
+
+# Install Node.js dependencies (if you have any)
+# npm install
+
+# Stop the previous running instance of your app (if any)
+# pm2 stop your-app-name
+
+# Start your Node.js application using a process manager like PM2
+# Make sure PM2 is installed globally: npm install -g pm2
+pm2 start app.js --name your-app-name
+
+# Optionally, you can monitor your app using PM2 by running:
+# pm2 monit
+
+# Optionally, you can set up PM2 to start your app automatically on system boot:
+# pm2 startup
+# pm2 save
