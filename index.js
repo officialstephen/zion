00cbf9a31e081d7db8e4cbbb7b8564c8f3e3bd49
Stephen Adegbola <adegbola@fisshboneandlestr.com>
2023-09-25 15:30:03 +0100

    Update stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ Hello: "World" });
+  res.send({ Hello: "World!" });
 });
 const port = process.env.PORT;
 
